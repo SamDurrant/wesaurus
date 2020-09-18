e09c3fd9e1e1f1c9d23894e5e3daa3e0e88d5c31
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-09-17 18:34:39 -0600

    add alphabet filter

---
+++ src/components/AlphabetFilter/AlphabetFilter.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './AlphabetFilter.css'
+import styled from 'styled-components'
+
+const alphabet = [
+  'All',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+]
+
+const StyledLetter = styled.span`
+  color: ${({ theme }) => theme.text};
+  background: ${({ theme }) => theme.gold};
+`
+
+function AlphabetFilter(props) {
+  return (
+    <div className="alphabet-filter">
+      {alphabet.map((letter, i) => (
+        <StyledLetter
+          key={i}
+          onClick={() => props.filterFor(letter)}
+          className="alphabet-letter"
+        >
+          {letter}
+        </StyledLetter>
+      ))}
+    </div>
+  )
+}
+
+export default AlphabetFilter
--- src/pages/Dictionary/Dictionary.js
+++ src/pages/Dictionary/Dictionary.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import WordDisplay from '../../components/WordDisplay/WordDisplay'
 import './Dictionary.css'
 import MainContext from '../../contexts/MainContext'
+import AlphabetFilter from '../../components/AlphabetFilter/AlphabetFilter'
 
 const StyledHeading = styled.div`
   color: ${({ theme }) => theme.text};
@@ -11,16 +12,37 @@ const StyledHeading = styled.div`
 
 function Dictionary() {
   const { words } = useContext(MainContext)
+  const [displayWords, setDisplayWords] = useState(words)
+
+  const filterForWords = (letter) => {
+    const filtered = words.filter((word) => {
+      if (letter === 'All') return word
+      return word.word.toUpperCase().startsWith(letter)
+    })
+    setDisplayWords(filtered)
+  }
+
   return (
-    <section className="section section-dictionary">
-      <StyledHeading className="announce-box">
-        <h1>Hello Sam!</h1>
-      </StyledHeading>
-      <section className="word-box">
-        {words.map((word) => (
-          <WordDisplay key={word.word_id} word={word.word} id={word.word_id} />
-        ))}
+    <section className="section-dictionary">
+      <section className="section dictionary-main">
+        <StyledHeading className="announce-box">
+          <h1>Hello Sam!</h1>
+        </StyledHeading>
+        <div className="word-box">
+          {displayWords
+            .sort((a, b) =>
+              a.word.toLowerCase() < b.word.toLowerCase() ? -1 : 1
+            )
+            .map((word) => (
+              <WordDisplay
+                key={word.word_id}
+                word={word.word}
+                id={word.word_id}
+              />
+            ))}
+        </div>
       </section>
+      <AlphabetFilter filterFor={filterForWords} />
     </section>
   )
 }
